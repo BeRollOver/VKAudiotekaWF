6ef515661653175ed891d6306e2d0fe8b86a43b8
rollover <mr.mustard@yandex.ru>
2015-07-21 00:08:51 +0300

    Добавил комменты, улучшил обработку ошибок, нет изменений функционала

--- VKAudiotekaWF/Form1.cs
+++ VKAudiotekaWF/Form1.cs
@@ -17,9 +17,12 @@ namespace VKAudiotekaWF
         public Form1()
         {
             InitializeComponent();
+            scope = Settings.Groups;
+            scope = Settings.Audio;
         }
 
         VkApi vk = new VkApi();
+        Settings scope;
         System.Collections.ObjectModel.ReadOnlyCollection<VkNet.Model.Group> groupsList;
         VkNet.Model.Group group;
         System.Collections.ObjectModel.ReadOnlyCollection<VkNet.Model.Attachments.Audio> audiosList;
@@ -30,20 +33,30 @@ namespace VKAudiotekaWF
         {
             try
             {
-                Settings scope = Settings.Groups;
-                scope = Settings.Audio;
+                // Заходим в ВК
                 vk.Authorize(4919033, userText.Text, passText.Text, scope);
                 var user = vk.Users.Get((long)vk.UserId, null, null);
+                enterLabel.Text = "Успешный вход";
 
+                // Получаем инфу о группах
                 groupsList = vk.Groups.Get(user.Id, true, GroupsFilters.Moderator);
                 foreach (var item in groupsList)
                     groupsListBox.Items.Add(item.Name);
+
+                // Включаем все нужные кнопки
                 groupsListBox.Enabled = true;
-                enterLabel.Text = "Успешный вход";
+            }
+            catch (VkNet.Exception.VkApiAuthorizationException)
+            {
+                enterLabel.Text = "Неправильный логин/пароль";
+            }
+            catch (VkNet.Exception.VkApiException)
+            {
+                MessageBox.Show("Сбой со стороны ВКонтакте");
             }
             catch
             {
-                enterLabel.Text = "Неправильный логин/пароль";
+                MessageBox.Show("Что-то пошло не так, пните Митю!");
             }
         }
 
@@ -51,17 +64,22 @@ namespace VKAudiotekaWF
         {
             try
             {
+                // После выбора группы чистим лист с альбомами от предыдущей группы
                 if (albumsListBox.Items.Count != 0)
                     foreach (var item in albumsList)
                         albumsListBox.Items.Remove(item.Title);
+
+                // Выбираем нужную группу, получаем список всех аудиозаписей и альбомов, выводим его
                 group = vk.Groups.GetById(groupsList[groupsListBox.SelectedIndex].Id, null);
                 audiosList = vk.Audio.GetFromGroup((long)group.Id, null, null, 0, 0);
                 albumsList = vk.Audio.GetAlbums(-group.Id, null, 0);
                 foreach (var item in albumsList)
                     albumsListBox.Items.Add(item.Title);
-                albumsListBox.Enabled = true;
+
+                // Включаем все нужные кнопки
                 if (albumsList.Count == 50) next50.Enabled = true;
                 urlidCheckBox.Enabled = true;
+                albumsListBox.Enabled = true;
             }
             catch (VkNet.Exception.AccessDeniedException)
             {
@@ -77,22 +95,26 @@ namespace VKAudiotekaWF
         {
             try
             {
+                // Удаляем все альбомы из списка перед обновлением
                 foreach (var item in albumsList)
                     albumsListBox.Items.Remove(item.Title);
 
+                // Получаем новые 50 альбомов
                 albumsCount += 50;
                 albumsList = new System.Collections.ObjectModel.ReadOnlyCollection<VkNet.Model.AudioAlbum>(vk.Audio.GetAlbums(-group.Id, null, albumsCount));
+
+                // Добавляем новые альбомы в список
+                foreach (var item in albumsList)
+                    albumsListBox.Items.Add(item.Title);
+
+                // Врубаем все нужные кнопки
                 if (albumsList.Count != 50) 
                     next50.Enabled = false;
                 prev50.Enabled = true;
-
-                
-                foreach (var item in albumsList)
-                    albumsListBox.Items.Add(item.Title);
             }
             catch
             {
-                MessageBox.Show("Больше нет альбомов");
+                MessageBox.Show("Альбомов больше нет");
             }
         }
 
@@ -100,18 +122,21 @@ namespace VKAudiotekaWF
         {
             try
             {
+                // Удаляем все альбомы из списка перед обновлением
                 foreach (var item in albumsList)
                     albumsListBox.Items.Remove(item.Title);
 
+                // Получаем предыдущие 50 альбомов
                 albumsCount -= 50;
                 albumsList = new System.Collections.ObjectModel.ReadOnlyCollection<VkNet.Model.AudioAlbum>(vk.Audio.GetAlbums(-group.Id, null, albumsCount));
-                if (albumsCount == 0)
-                    prev50.Enabled = false;
-                if (albumsList.Count == 50) 
-                    next50.Enabled = true;
 
+                // Добавляем полученные альбомы в список
                 foreach (var item in albumsList)
-                    albumsListBox.Items.Add(item.Title);
+                    albumsListBox.Items.Add(item.Title); 
+                
+                // Врубаем нужные кнопки
+                if (albumsCount == 0) prev50.Enabled = false;
+                if (albumsList.Count == 50) next50.Enabled = true;
             }
             catch
             {
@@ -119,9 +144,27 @@ namespace VKAudiotekaWF
             }
         }
 
+        // Активизирует конпку Поднять при выборе альбома
         private void albumsListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             up.Enabled = true;
+        }
+
+        // По URL и ID
+        private void urlidCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (urlidCheckBox.Checked)
+            {
+                albumsListBox.Enabled = false;
+                urlidTextBox.Visible = true;
+                up.Enabled = true;
+            }
+            else
+            {
+                albumsListBox.Enabled = true;
+                urlidTextBox.Visible = false;
+                if (albumsListBox.SelectedItem == null) up.Enabled = false;
+            }
         }
 
         private void up_Click(object sender, EventArgs e)
@@ -129,6 +172,7 @@ namespace VKAudiotekaWF
             
             try
             {
+                // Для URL/ID
                 int a = 0;
                 try
                 {
@@ -145,40 +189,27 @@ namespace VKAudiotekaWF
 
                 // Поднимает указанный альбом на самый верх
                 var audioGroupAlbum = vk.Audio.GetFromGroup((long)group.Id,
-                    urlidCheckBox.Checked ?
-                    a
-                    : Convert.ToInt32(albumsList[albumsListBox.SelectedIndex].AlbumId), 
+                    urlidCheckBox.Checked ? a : Convert.ToInt32(albumsList[albumsListBox.SelectedIndex].AlbumId), //Определяет, брать альбом из списка или из textbox
                     null, 0, 0);
+
+                // Лучше не двигать альбом, если он первый
                 if (audioGroupAlbum[0].Id == audiosList[0].Id)
                 {
                     MessageBox.Show("Альбом и так первый");
                     return;
                 }
+
+                // Поднимаем
                 for (int j = 0; j < audioGroupAlbum.Count(); j++)
                     vk.Audio.Reorder(audioGroupAlbum[j].Id, -(long)group.Id, 0, audiosList[0].Id);
+
+                // Получаем новый список аудизаписей с новый порядком 
                 audiosList = vk.Audio.GetFromGroup((long)group.Id, null, null, 0, 0);
                 MessageBox.Show("Альбом поднят");
             }
             catch
             {
                 MessageBox.Show("Невозможно поднять альбом");
-            }
-        }
-
-        // По URL и ID
-        private void urlidCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            if (urlidCheckBox.Checked)
-            {
-                albumsListBox.Enabled = false;
-                urlidTextBox.Visible = true;
-                up.Enabled = true;
-            }
-            else
-            {
-                albumsListBox.Enabled = true;
-                urlidTextBox.Visible = false;
-                if(albumsListBox.SelectedItem == null) up.Enabled = false;
             }
         }
     }
